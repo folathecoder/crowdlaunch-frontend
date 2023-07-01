c31baf236f3a9c1b40bbfcc72ec8b7f1e1f9f087
folathecoder <folathecoder@gmail.com>
2023-07-01 21:46:15 +0100

    Implemented app footer component

--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,5 +1,6 @@
 import Header from '@/components/global/header/header';
+import Footer from '@/components/global/footer/footer';
 import Button from '@/components/global/button/button';
 import CartButton from '@/components/global/button/cartButton';
 
-export { Header, Button, CartButton };
+export { Header, Button, CartButton, Footer };
